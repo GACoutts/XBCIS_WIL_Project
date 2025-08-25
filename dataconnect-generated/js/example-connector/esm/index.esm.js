afccfa68842c52298ccb12d8f1c0fff84b85855c
GACoutts <ST10258297@imconnect.edu.za>
2025-08-25 12:28:49 +0200

    Firebase -Firebase has been added but still needs set up -Does not have ability to save yet -dataconnect and dataconnect-genertae are mock data examples that were auto genertaed and as far as i can see not needed

---
+++ dataconnect-generated/js/example-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'example',
+  service: 'xbciswilproject',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/example-connector/index.cjs.js
@@ -0,0 +1,7 @@
+
+const connectorConfig = {
+  connector: 'example',
+  service: 'xbciswilproject',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
