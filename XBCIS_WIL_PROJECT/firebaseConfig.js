c37d8903fff1c31492d1c1f9a5ee3411072d3c55
GACoutts <ST10258297@imconnect.edu.za>
2025-09-04 23:59:35 +0200

    Notes changed

---
+++ XBCIS_WIL_PROJECT/firebaseConfig.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// can add SDK for tools
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+//Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDOGrhK2-YtPiZgWL9-jGsLSXfGrNZe4ik",
+  authDomain: "rawsoncloudstorage.firebaseapp.com",
+  projectId: "rawsoncloudstorage",
+  storageBucket: "rawsoncloudstorage.firebasestorage.app",
+  messagingSenderId: "65886494589",
+  appId: "1:65886494589:web:629a22e2cb5ae423e93614",
+  measurementId: "G-MF6SW8K25F"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
